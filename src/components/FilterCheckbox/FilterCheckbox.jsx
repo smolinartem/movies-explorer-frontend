62f4f0cdd42f974d5cc0b40ac2797226626bb210
Artem Smolin <krutopognali@yandex.ru>
2023-11-13 21:51:20 +0300

    feat: lang, checkbox, search input validation

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -1,17 +1,10 @@
 import './FilterCheckbox.css'
 
 function FilterCheckbox() {
-  const handleChange = () => {}
   return (
     <fieldset className='checkbox'>
       <div className='checkbox__toggle'>
-        <input
-          className='checkbox__input'
-          name='checkbox-film'
-          type='checkbox'
-          checked
-          onChange={handleChange}
-        />
+        <input className='checkbox__input' name='checkbox-film' type='checkbox' />
         <label className='checkbox__lable' />
       </div>
       <span className='checkbox__title'>Короткометражки</span>
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,20 +1,26 @@
 import './SearchForm.css'
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox'
 import Button from '../Button/Button'
+import { useState } from 'react'
 
 function SearchForm() {
+  const [isValid, setIsValid] = useState(true)
   const handleSubmit = (event) => {
     event.preventDefault()
+
+    const form = event.target
+    setIsValid(form.checkValidity())
   }
   return (
-    <form className='search' onSubmit={handleSubmit}>
+    <form className='search' onSubmit={handleSubmit} noValidate>
       <fieldset className='search__info'>
         <input
-          className='search__input'
+          className={`search__input ${isValid ? '' : 'search__input_invalid'}`}
           name='search-film'
           placeholder='Фильм'
           type='search'
           autoComplete='off'
+          required
         />
         <Button className='search__submit' type='submit' />
       </fieldset>
